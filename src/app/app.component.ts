16869143ca8911a835aa1997b359a68b6ad0dcd2
Jaime Navarrete <jaimenava123@outlook.com>
2020-08-29 16:29:03 -0600

    Completado el diseño de todos los elementos de la pagina

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,48 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'desafiopractico';
+  title = 'Desafio Practico 1 - Jaime Navarrete';
+
+  registros = [];
+
+  activarAlerta(ubicacion:string) {
+    let alerta = document.querySelector(ubicacion + ' .alert');
+
+    alerta.classList.add('active');
+
+    setTimeout(() => {
+      alerta.classList.remove('active');
+    }, 2000);
+  }
+
+  agregarRegistro() {
+    const listaRegistros = document.getElementById('listaRegistros'),
+          nombre = document.getElementById('nombre') as HTMLInputElement,
+          dui = document.getElementById('dui') as HTMLInputElement,
+          vehiculo = document.getElementById('vehiculo') as HTMLInputElement,
+          costo = document.getElementById('costo') as HTMLInputElement,
+          cantRegistros = this.registros.length;
+
+    if(nombre.value && dui.value && vehiculo.value && costo.value) {
+      this.registros.push({id:cantRegistros, nombre: nombre.value, dui: dui.value, vehiculo: vehiculo.value, costo: costo.value});
+
+      let registro = document.createElement('div');
+      registro.classList.add('card', 'mb-4');
+      registro.innerHTML = `<div class="card-body">
+          <h5 class="card-title">Registro de visita ${cantRegistros + 1}</h5>
+          <h6 class="card-subtitle text-muted pb-3 mb-4">Reparación de vehículo</h6>
+
+          <p class="card-text"><strong>Cliente</strong>: ${nombre.value} <strong>DUI</strong>: ${dui.value}</p>
+          <p class="card-text"><strong>Vehículo</strong>: ${vehiculo.value}</p>
+          <p class="card-text"><strong>Costo de reparación</strong>: ${costo.value}</p>
+        </div>`;
+
+      listaRegistros.appendChild(registro);
+
+      this.activarAlerta('#listaRegistros');
+    }
+    else {
+      this.activarAlerta('#formCliente');
+    }
+  }
 }
